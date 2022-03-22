21a262c5d1f39b5be1296a0de7bb936d88fb2f76
startup-ideas-project <randomstartupproject2021@gmail.com>
2022-03-21 21:01:17 -0500

    improve landing page

--- src/landing-page/hero.js
+++ src/landing-page/hero.js
@@ -1,21 +1,22 @@
-import React, { useRef, useEffect } from 'react'
+import React, { useRef, useEffect, useState } from 'react'
 import ScrollReveal from 'scrollreveal'
 import emailjs from 'emailjs-com';
 
 import { NewsletterForm } from './newsletter-form'
 
-export function Hero({ title, subTitle, content1, content2, illustration: Illustration }) {
+export function Hero({ title, subTitle, content1, content2, content3, illustration: Illustration }) {
   const scrollRevealRef = useRef([])
+  const [emailReceived, setEmailReceived] = useState(false)
 
   useEffect(() => {
     if (scrollRevealRef.current) {
       scrollRevealRef.current.map((ref, index) =>
-        ScrollReveal().reveal(scrollRevealRef.current[index], {
-          duration: 1000,
+        ScrollReveal().reveal('.reveal', {
+          duration: 6000,
           distance: '40px',
           easing: 'cubic-bezier(0.5, -0.01, 0, 1.005)',
           origin: 'top',
-          interval: 150,
+          interval: 2500,
         })
       )
     }
@@ -29,33 +30,49 @@ export function Hero({ title, subTitle, content1, content2, illustration: Illust
         <div className="hero-inner relative lg:flex">
           <div className="hero-copy bg-white pt-10 pb-16 lg:pt-16 lg:pr-20" style={{ minWidth: '600px' }}>
             <div className="mx-auto w-full max-w-3xl">
-              <h1 ref={(el) => (scrollRevealRef.current[0] = el)} className="mt-0 mb-4 text-4xl font-bold md:text-5xl " style={{color:'purple'}}>
+              <h1 ref={(el) => (scrollRevealRef.current[1] = el)} className="mt-0 mb-4 text-4xl font-bold md:text-5xl reveal" style={{color:'purple'}}>
                 {title}
               </h1>
-              <h1 ref={(el) => (scrollRevealRef.current[0] = el)} className="mt-0 mb-4 text-4xl font-bold md:text-5xl "  style={{color:'#5850EC'}} >
+              <h1 ref={(el) => (scrollRevealRef.current[3] = el)} className="mt-0 mb-4 text-4xl font-bold md:text-5xl reveal"  style={{color:'#5850EC'}} >
                 {subTitle}
               </h1>
-              <p ref={(el) => (scrollRevealRef.current[1] = el)} className="prose prose-xl px-16 text-gray-500 md:px-0" >
+              <br/>
+              <p ref={(el) => (scrollRevealRef.current[3] = el)} className="prose prose-xl px-16 text-gray-500 md:px-0 reveal" >
                 {content1}
               </p>
-              <p ref={(el) => (scrollRevealRef.current[1] = el)} className="prose prose-xl px-16 text-gray-500 md:px-0" >
+              <br/>
+              <p ref={(el) => (scrollRevealRef.current[3] = el)} className="prose prose-xl px-16 text-gray-500 md:px-0 reveal" >
                 {content2}
               </p>
+              <br/>
+              <p className="prose prose-xl px-16 text-gray-500 md:px-0 reveal" >
+                {content3}
+              </p>
+              <br/>
             </div>
-
-            <div ref={(el) => (scrollRevealRef.current[2] = el)}>
+            <div ref={(el) => (scrollRevealRef.current[0] = el)}>
+            <h4 style={{color:'red'}} className="reveal text-gray-500">
+                Beta is coming in 4 weeks
+            </h4>
               <NewsletterForm
-                className="m-0 mt-8 max-w-md md:flex"
+                className="m-0 mt-8 max-w-md md:flex reveal"
                 submitBtn="Get early access"
                 onSubmit={(values) => {
                     emailjs.send('service_kjcd90a', 'template_yhqddaf', {
                         from_name: "landing page",
                         to_name: "hoang le",
                         message: values,
-                    }, 'jNYbwsP9Nt_uBpl-c')
+                    }, 'jNYbwsP9Nt_uBpl-c').then(_ => setEmailReceived(true))
                 }}
               />
+              
             </div>
+
+          {emailReceived && 
+          <div>
+              <h4 style={{color:'green'}}>Congratulations! we got your email</h4>
+          </div>
+          }
           </div>
 
           <div className="relative -ml-6 -mr-6 py-10 lg:p-0">
--- src/landing-page/landing-page.js
+++ src/landing-page/landing-page.js
@@ -13,8 +13,9 @@ const LandingPage = () => {
         <Hero
         title="Access external data"
         subTitle="The Easy Way"
-        content1="For data provider, generate revenue from your data"
-        content2="For data consumer, obtain desired data"
+        content1="1. For data provider, generate revenue from your data"
+        content2="2. For data consumer, you see data quality before you buy"
+        content3="3. Leave an email, you could be the first one to experience"
         illustration={HeroIllustration}
         />
     </Layout>
