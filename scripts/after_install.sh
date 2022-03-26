a26e2222c27edce646feee5fdb6223ba480295c1
startup-ideas-project <randomstartupproject2021@gmail.com>
2022-03-26 01:02:45 -0500

    try this script

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-cd /home/ec2-user/server
+
+# navigate to app folder
+cd /app
+
+# install dependencies
 npm install
+
+# install create-react-app and react-scripts
+# without react-scripts application cannot be started
 npm install --save create-react-app react-scripts
+
+# install pm2 process manager
 npm install pm2 -g
--- scripts/app_start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd /home/ec2-user/server/src
-npm start
-pm2 start npm --name "hippo-frontend" -- start
-pm2 startup
-pm2 save
-pm2 restart all
