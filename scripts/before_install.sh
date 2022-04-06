7992cd2aae6f496021459b1c2043f856a0b526fc
startup-ideas-project <randomstartupproject2021@gmail.com>
2022-04-06 14:42:51 -0500

    give permission to after_install

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -6,3 +6,6 @@ cd /app
 # install node and npm
 curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -
 yum -y install nodejs npm
+
+# Change after install permission
+chmod 777 after_install.sh
