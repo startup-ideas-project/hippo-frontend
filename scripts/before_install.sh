d3397ff71691bb7aafe9c44b09e669a8d0f7ce8a
startup-ideas-project <randomstartupproject2021@gmail.com>
2022-04-07 11:45:11 -0500

    ubuntu update

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -4,5 +4,7 @@
 cd /home/ec2-user/server
 
 # install node and npm
-curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -
-sudo apt-get -y install nodejs npm
+sudo apt install curl
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+sudo apt -y install nodejs npm
+
